ac8be1d8dbbf34988946bfdc8211c66c25879880
Tom Jeleniewski <79652838+jelenito@users.noreply.github.com>
2025-08-13 17:27:28 +0200

    Improved SPARQL error handling

--- src/sparqlClient.ts
+++ src/sparqlClient.ts
@@ -7,7 +7,8 @@ import axios from 'axios';
  * @returns JSON-Ergebnis der Query
  */
 export async function runSelectQuery(query: string, endpoint: string): Promise<any> {
-   // console.log('📤 Fired SPARQL QUERY:\n', query);  // 
+  
+   try{
     const response = await axios.post(
       endpoint,
       `query=${encodeURIComponent(query)}`,
@@ -20,5 +21,10 @@ export async function runSelectQuery(query: string, endpoint: string): Promise<a
     );
   
     return response.data;
+   } catch (error: any) {
+    console.error('Failed SPARQL query:\n', query);
+    const status = error.response?.status;
+    throw new Error(`SPARQL request to ${endpoint} failed with status ${status}. Query: ${query}`);
   }
+}
   
